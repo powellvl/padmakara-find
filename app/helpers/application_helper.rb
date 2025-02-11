424df54f248d516a426c21b0064f3e115369c805
powellvl <lucax.mb03@gmail.com>
2025-02-11 16:36:21 +0100

    Adds system of icons depending on file type (pdf, doc, indd)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def is_last?(index, collection)
     index == collection.size - 1
   end
+
+  def file_icon_path(filename)
+    extension = File.extname(filename.to_s).downcase.delete(".")
+    case extension
+    when "doc", "docx"
+      "icons/doc-file.svg"
+    when "indd"
+      "icons/indd-file.svg"
+    when "pdf"
+      "icons/pdf-file.svg"
+    else
+      nil
+    end
+  end
 end
