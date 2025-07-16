2a0cc35015fda6442361bfa83ca3fa8b8c70d6ca
Jérémy FRERE <frere.jeremy@gmail.com>
2025-07-16 16:08:08 +0200

    Adds some seeds for for schools, authors, tags and deities

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+%w[Kagyü Nyingma Gelug Sakya].each do |name|
+  School.find_or_create_by!(name: name)
+end
+
+[
+  "Patrül Rinpoche", "Jamyang Khyentse Wangpo", "Jamyang Khyentse Chökyi Lodrö",
+  "Jamgön Kongtrül Lodrö Thayé", "Ngawang Palzang",
+  "Kangyur Rinpoche", "Dilgo Khyentse Rinpoche", "Dudjom Rinpoche",
+  "Jigme Khyentse Rinpoche", "Pema Wangyal Rinpoche", "Tsongkhapa"
+].each do |name|
+  Author.find_or_create_by!(name_english: name)
+end
+
+[ "Sadhana", "Préliminaires (Ngöndro)", "Entraînement de l'esprit (Lojong)", "Confession", "Protecteurs" ].each do |name|
+  Tag.find_or_create_by!(name: name)
+end
+
+[ "Shakyamuni Buddha", "Chenrezi", "Amitabha", "Amitayus", "Tara", "Manjushri", "Vajrapani", "Vajrakilaya", "Guru Rinpoche" ].each do |name|
+  Deity.find_or_create_by!(name_english: name)
+end
